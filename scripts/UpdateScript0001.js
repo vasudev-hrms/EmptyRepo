e65be5568b9317e92101ed88226d9d8dd990f14a
Hosalli <vasudev-hrms@users.noreply.github.com>
2016-07-11 16:30:22 +0530

    test

---
+++ scripts/UpdateScript0001.js
@@ -0,0 +1,10 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ //my comments to all the scripts
+//my updated data
+//my comments to all the scripts
+//my comments to all the scripts
+//my comments to all the scriptssd			
